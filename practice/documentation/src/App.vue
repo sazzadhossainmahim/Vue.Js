cc86ea83bb4bd5512f92bc8efce12532e638b495
sazzadhosssainmahim <44097425+sazzadhossainmahim@users.noreply.github.com>
2022-04-18 03:22:04 +0600

    Documentation Building

---
+++ practice/documentation/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import './index.css';
+</script>
+
+<template>
+  <div class="box-border w-screen h-64 p-2 m-2 text-center border border-green-400 rounded">
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<style>
+
+
+</style>
---
+++ practice/documentation/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './index.css'
+
+
+// Template Syntax 
+
+createApp(App).mount('#app', )
