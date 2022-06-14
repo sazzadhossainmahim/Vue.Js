6952ad033219cfef338a577dc4370f931f27daa8
sazzadhosssainmahim <44097425+sazzadhossainmahim@users.noreply.github.com>
2022-06-14 20:15:30 +0600

    ./

--- practice/documentation/src/main.js
+++ practice/documentation/src/main.js
@@ -2,12 +2,22 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 
+// app.mount('#app')
 const app = createApp({
     data() {
         return {
-            message: 0
+            count: 0
         }
     }
 })
+createApp(App).mount('#app')
 
-app.mount('#app')
+const app1 = createApp({
+    /* ... */
+})
+app1.mount('#container-1')
+
+const app2 = createApp({
+    /* ... */
+})
+app2.mount('#container-2')
