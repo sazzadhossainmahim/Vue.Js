f449984a33e9df5b8ed6179be68319b56580f401
sazzadhosssainmahim <44097425+sazzadhossainmahim@users.noreply.github.com>
2022-07-04 03:26:10 +0600

    Update app.vue

--- practice/tutorial/src/App.vue
+++ practice/tutorial/src/App.vue
@@ -68,21 +68,157 @@
   </div> -->
 
   <!-- Event Handling -->
-  <div>
-  <button v-on:click="name = 'bruce'">ChangeEventName</button>
+  <!-- <div>
+    <button v-on:click="name = 'bruce'">ChangeEventName</button>
     <h2>{{ name }}</h2>
     <h2>{{ eventCountValue }}</h2>
     <button v-on:click="eventHandlingIncrement(4)">EventIncrement</button>
     <button v-on:click="eventHandlingDecrement(3)">EventDecrement</button>
     <button v-on:click="eventHandlingIncrement(2)">EventIncrement</button>
     <button @click="eventHandlingDecrement(1)">EventDecrement</button>
-    
-  </div>
+  </div> -->
+
+  <!-- -------------Form Handling -->
+  <!-- <div> -->
+  <!-- <pre>
+    {{ JSON.stringify(formHandlingValues, null, 2) }}
+    </pre> -->
+  <!-- </div> -->
+  <!-- <div style="margin: 2px; padding: 2px"> -->
+  <!-- <form @submit.prevent="submitForm">
+      <div>
+        <label for="name">Name</label>
+        <input type="text" id="name" v-model.trim.lazy="formHandlingValues.name" />
+      </div>
+      <div>
+        <label for="profile">Profile Summery</label>
+        <textarea
+          name=""
+          id="profile"
+          cols="30"
+          rows="10"
+          v-model="formHandlingValues.profileSummery"
+        ></textarea>
+      </div>
+
+      <div>
+        <label for=""></label>
+        <select name="" id="country" v-model="formHandlingValues.country">
+          <option value="">select country</option>
+          <option value="india">india</option>
+          <option value="bd">bd</option>
+          <option value="pak">pak</option>
+        </select>
+      </div>
+      <div>
+        <label for=""></label>
+        <select
+          name=""
+          id="jobLocation"
+          multiple
+          v-model="formHandlingValues.jobLocation"
+        >
+          <option value="">select job</option>
+          <option value="india">india</option>
+          <option value="bd">bd</option>
+          <option value="pak">pak</option>
+        </select>
+      </div>
+
+      <div>
+        <input
+          type="checkbox"
+          name=""
+          id="remoteWork"
+          v-model="formHandlingValues.remoteWork"
+          true-value="yes"
+          true-false="no"
+        />
+
+        <label for="remoteWork">Open to remote work</label>
+      </div> -->
+
+  <!-- Checkbox group -->
+  <!-- <div>
+        <label for="">Skill Set</label>
+        <input
+          type="checkbox"
+          name=""
+          id="html"
+          value="javascript"
+          v-model="formHandlingValues.skillSet"
+        />
+        <label for="remoteWork">CSS</label>
+        <input
+          type="checkbox"
+          name=""
+          id="css"
+          value="css"
+          v-model="formHandlingValues.skillSet"
+        />
+        <label for="remoteWork">javascript</label>
+        <input
+          type="checkbox"
+          name=""
+          id="javascript"
+          value="javascript"
+          v-model="formHandlingValues.skillSet"
+        />
+      </div> -->
+  <!-- radio button -->
+  <!-- <div>
+        <label for="">0-2</label>
+        <input
+          type="radio"
+          name=""
+          id="0-2"
+          value="0-2"
+          v-model="formHandlingValues.yearsOfExperience"
+        />
+        <label for="remoteWork">3-5</label>
+        <input
+          type="radio"
+          name=""
+          id="3-5"
+          value="3-5"
+          v-model="formHandlingValues.yearsOfExperience"
+        />
+        <label for="remoteWork">10</label>
+        <input
+          type="radio"
+          name=""
+          id="6-10"
+          value="6-10"
+          v-model="formHandlingValues.yearsOfExperience"
+        />
+      </div> -->
+  <!-- ---------------------------------------------Modifiers -->
+
+  <!-- <div>
+      <label for="">Age</label>
+      <input @keyup.enter="submitForm" type="number" name="" id="age" v-model="formHandlingValues.modifierAge">
+      </div> -->
+  <!--  -->
+
+  <!-- <div><button>Submit</button></div> -->
+  <!-- </form> -->
+  <!-- </div>  -->
+
+  <!-- Bonus Directive -->
+  <!-- <div>
+    <h2 v-once>{{bonusDirectivesName}}</h2>
+    <button @click="bonusDirectivesName ='i am sam'">Change Name</button>
+    <h2 v-pre>{{bonusDirectivesName}}</h2>
+  </div> -->
+
+  <!-- Computed Property -->
+  <div>FullName - {{ computedFirstName }}-{{ computedLastName }}</div>
+  <h1>{{ fullName }}</h1>
+  <button @click="computedPropertyItems.push({id:4,title:'keyboard',price:50})">Add Item </button>
+  <h2>
+    Total - {{total}}
+  </h2>
 </template>
-
-
-
-
 
 <script>
 export default {
@@ -90,40 +226,71 @@ export default {
 
   data() {
     return {
-      greet: "hello",
-      name: "batman",
-      c: "code",
-      BindingHtml: "<b>code</b>",
-      BindAttribute: "Heading",
-      ButtonIsDisable: true,
-      DynamicBing: "danger",
+      // greet: "hello",
+      // name: "batman",
+      // c: "code",
+      // BindingHtml: "<b>code</b>",
+      // BindAttribute: "Heading",
+      // ButtonIsDisable: true,
+      // DynamicBing: "danger",
       // inline style
-      InlineBindingStyleColor: "orange",
-      InlineBindingStyleHeader: 20,
-      ObjectStyleHeader: {
-        color: "red",
-        padding: "20px",
-      },
-      ObjectStyleObject: {
-        color: "green",
-        backgroundColor: "lightgreen",
-        border: " 1px solid green",
-      },
+      // InlineBindingStyleColor: "orange",
+      // InlineBindingStyleHeader: 20,
+      // ObjectStyleHeader: {
+      //   color: "red",
+      //   padding: "20px",
+      // },
+      // ObjectStyleObject: {
+      //   color: "green",
+      //   backgroundColor: "lightgreen",
+      //   border: " 1px solid green",
+      // },
       // Conditional Rendering
-      ConditionIfNum: -5,
-      vShow: true,
-      vShowElement: true,
-
+      // ConditionIfNum: -5,
+      // vShow: true,
+      // vShowElement: true,
       // v-for Directive
-      ListRenderings: ["hi", "man", "hey"],
+      // ListRenderings: ["hi", "man", "hey"],
       // Key Attribute
-      keyAttributes: ["bruce", "clark", "diana"],
+      // keyAttributes: ["bruce", "clark", "diana"],
       // conditional list rendering
-      conditionalListElements: ["Bruce", "clark"],
-
+      // conditionalListElements: ["Bruce", "clark"],
       // Event Handling
-      eventCountValue: 0,
-      eventCount: 0,
+      // eventCountValue: 0,
+      // eventCount: 0,
+      // ------------Form Handling
+      // formHandlingValues: {
+      //   name: "",
+      //   profileSummery: "",
+      //   country: "",
+      //   jobLocation: [],
+      //   remoteWork: "no",
+      //   skillSet: [],
+      //   yearsOfExperience: "",
+      //   modifierAge: null,
+      //   modifierEnterKey:'',
+      // },
+      // -----------------Bonus Directives
+      bonusDirectivesName: "i am sam ",
+      computedFirstName: "sam",
+      computedLastName: "Wayne",
+      computedPropertyItems: [
+        {
+          id: 1,
+          title: "TV",
+          price: 100,
+        },
+        {
+          id: 2,
+          title: "Phone",
+          price: 200,
+        },
+        {
+          id:3,
+          title:'Laptop',
+          price: 300,
+        },
+      ],
     };
   },
 
@@ -136,13 +303,31 @@ export default {
       return a + b + c;
     },
     // Event Handling
-    eventHandlingIncrement(eventIncrementNum){
-        this.eventCountValue +=eventIncrementNum
-      },
-    eventHandlingDecrement(eventIncrementNum){
-        this.eventCountValue -=eventIncrementNum
-      }
+    eventHandlingIncrement(eventIncrementNum) {
+      this.eventCountValue += eventIncrementNum;
+    },
+    eventHandlingDecrement(eventIncrementNum) {
+      this.eventCountValue -= eventIncrementNum;
+    },
+
+    // Submit From Handle
+    submitForm() {
+      // event.preventDefault();
+      console.log("Form values", this.formHandlingValues);
+    },
   },
+  computedFirstName: {
+    fullName() {
+      return `${this.computedFirstName} ${this.computedLastName}`;
+    },
+  },
+  computed:{
+      // ComputedProperty
+  total(){
+    return this.computedPropertyItems.reduce((total,curr)=>(total=total+curr.price),0)
+  }
+}
+
 };
 </script>
 j
