5b0476d5ba44c98f0940b1e4680a05ad30379239
sazzadhosssainmahim <44097425+sazzadhossainmahim@users.noreply.github.com>
2022-07-17 01:37:47 +0600

    ./

---
+++ Project/Starter Project/calculator/src/components/Calculator.vue
@@ -0,0 +1,110 @@
+<template>
+  <div
+    class="
+      grid grid-cols-4
+      bg-slate-200
+      rounded
+      border border-black
+      m-auto
+      w-52
+    "
+  >
+    <div id="" class="col-span-4 bg-red-100 text-4xl border border-black">
+      {{ current || "0" }}
+    </div>
+    <div class="border border-black" @click="clear">C</div>
+    <div class="border border-black" @click="sign">+/-</div>
+    <div class="border border-black" @click="percent">%</div>
+    <div class="border border-black bg-orange-400">/</div>
+    <div class="border border-black" @click="append('7')">7</div>
+    <div class="border border-black" @click="append('8')">8</div>
+    <div class="border border-black" @click="append('9')">9</div>
+    <div class="border border-black bg-orange-400" @click="times">x</div>
+    <div class="border border-black" @click="append('4')">4</div>
+    <div class="border border-black" @click="append('5')">5</div>
+    <div class="border border-black" @click="append('6')">6</div>
+    <div class="border border-black bg-orange-400" @click="minus">-</div>
+    <div class="border border-black" @click="append('1')">1</div>
+    <div class="border border-black" @click="append('2')">2</div>
+    <div class="border border-black" @click="append('3')">3</div>
+    <div class="border border-black bg-orange-400" @click="add">+</div>
+    <div class="border border-black col-span-2" @click="append('0')">0</div>
+    <div class="border border-black" @click="dot">.</div>
+    <div class="border border-black bg-orange-400" @click="equal"> =</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    // msg: String
+  },
+  data() {
+    return {
+      current: "",
+      previous: null,
+      operatorClicked: false,
+      operator: null,
+    };
+  },
+  methods: {
+    clear() {
+      this.current = "";
+    },
+    sign() {
+      this.current =
+        this.current.charAt(0) === "-"
+          ? this.current.slice(1)
+          : `-${this.current}`;
+    },
+    percent() {
+      this.current = `${parseFloat(this.current) / 100}`;
+    },
+    append(number) {
+      if (this.operatorClicked) {
+        this.current = '';
+        this.operatorClicked = false;
+      }
+      this.current = `${this.current}${number}`;
+    },
+    dot() {
+      if (this.current.indexOf('.') === -1) {
+        this.append('.');
+      }
+    },
+
+    setPrevious() {
+      this.previous = this.current;
+      this.operatorClicked = true;
+    },
+    divide() {
+      this.operator = (a, b) => a / b;
+      this.setPrevious();
+    },
+    times() {
+      this.operator = (a, b) => a * b;
+      this.setPrevious();
+    },
+    add() {
+      this.operator = (a, b) => a + b;
+      this.setPrevious();
+    },
+    minus() {
+      this.operator = (a, b) => a - b;
+      this.setPrevious();
+    },
+    equal() {
+      this.current = `${this.operator(
+        parseFloat(this.current),
+        parseFloat(this.previous)
+      )}`;
+      this.previous = null;
+    },
+  },
+};
+</script>
+
+
+<style scoped>
+</style>
